54a9a7a4381f45284fc011f0126f94a2e0cb1837
mbayajames <jmuthiora244@gmail.com>
2025-03-10 01:14:07 -0700

    Update UI components in App.js for improved user experience

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import AdminDashboard from './components/AdminDashboard';
 import StudentDashboard from './components/StudentDashboard';
 import Sidebar from './components/Sidebar';
@@ -7,23 +7,22 @@ import Login from './components/Login';
 import PrivateRoute from './utils/PrivateRoute';
 import './App.css';
 
-
 const App = () => {
   return (
     <Router>
-       <div className="app">
-            <Sidebar/>
-            <div className="main-content">
-                <Routes>
-                  <Route path="/login" element={<Login />} />
-                  <Route element={<PrivateRoute />}>
-                     <Route path="/admin" element={<AdminDashboard />} />
-                     <Route path="/student" element={<StudentDashboard studentId="12345" />} />
-                  </Route>
-                  <Route path="/" element={<Navigate to="/login" />} />
-                </Routes>
-            </div>
-       </div>
+      <div className="app">
+        <Sidebar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="/admin" element={<AdminDashboard />} />
+              <Route path="/student" element={<StudentDashboard studentId="12345" />} />
+            </Route>
+            <Route path="/" element={<Navigate to="/login" />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 };
